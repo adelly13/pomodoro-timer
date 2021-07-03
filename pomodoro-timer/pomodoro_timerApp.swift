34c677afc447dbb3f3498d83860ed875d238c04f
Adelina C <73216866+adelly13@users.noreply.github.com>
2021-07-03 12:32:26 -0700

    Initial Commit

---
+++ pomodoro-timer/pomodoro_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_timerApp.swift
+//  pomodoro-timer
+//
+//  Created by Adelina C on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct pomodoro_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
